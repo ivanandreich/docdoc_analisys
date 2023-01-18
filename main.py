0136c52e26a3b51e4b8dc69db2dd8d7e68c57151
ivanandreich <iwanarmadillosolo1@gmail.com>
2023-01-18 09:35:51 +0400

    Got idea how to parse services

--- main.py
+++ main.py
@@ -15,18 +15,31 @@ main_url = config["Url"]["main_url"]
 doctors_url = config["Url"]["doctors_url"]
 clinics_url = config["Url"]["clinics_url"]
 
-r = requests.get('https://izh.docdoc.ru/clinic/lada_estet_na_karla_marksa')
-soup = bs(r.text, "html.parser")
-clinics_tag = soup.find_all('clinic-page')
+# r = requests.get('https://izh.docdoc.ru/clinic/ekomed')
+save_html('https://izh.docdoc.ru/clinic/ekomed', 'ekomed.html')
+# soup = bs(r.text, "html.parser")
+# clinics_tag = soup.find_all('clinic-page')
+#
+# clinics_tag = str(clinics_tag[0])
+#
+# diag = clinics_tag[clinics_tag.find(':diagnostics-services=') + len(':diagnostics-services=') + 1:clinics_tag.find(':is-clinic-active="true"')-2]
+# med = clinics_tag[clinics_tag.find(':med-services=') + len(':med-services=') + 1:clinics_tag.find(':total-reviews-count="35"')-2]
+# full = diag[:len(diag)-1] + ',' + med[1:]
+# print(full)
+# # file = open("services.json", "w", encoding='cp1251')
+# # l=json.loads(diag)
+# # print(l)
+# # json.dump(file, l)
+# # file.write(json.dump(l, ensure_ascii=False))
+# # file.close()
+#
+# services = json.loads(full)
+# print(type(services))
+#
+# with open('services.json', 'w', encoding='utf-8') as fp:
+#     json.dump(services, fp)
 
-clinics_json = str(clinics_tag[0])
-
-diag = clinics_json[clinics_json.find(':diagnostics-services='):clinics_json.find('class="adaptive-clinic-page"')]
-med = clinics_json[clinics_json.find(':med-services='):]
-print(clinics_json)
-services_dict = {'diag': diag
-                 'med': med}
-
+# print(services_dict)
 # fill_doctors_json(doctors_url)
 # fill_clinics_json(clinics_url)
 # print(fill_specs_dict_list("doctors_json.json"))
